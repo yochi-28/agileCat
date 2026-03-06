c589c6983407ee218c38e38d182ee1ed5048ff72
ManojMIS <manojkumar.k2023@gmail.com>
2026-03-06 16:17:56 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# main.py
+
+def main():
+    print("Hello, Jenkins! This is the initial version of the code.")
+    print("Pipeline execution should pull this file.")
+
+if __name__ == "__main__":
+    main()
